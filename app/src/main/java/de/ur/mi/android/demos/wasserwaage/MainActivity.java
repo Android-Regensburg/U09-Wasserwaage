e1cf762b7a9c51ace915c0e8cdc882faafae7d6e
Alexander Bazo <alexanderbazo@googlemail.com>
2021-06-10 08:47:19 +0200

    Prepare starter code

--- app/src/main/java/de/ur/mi/android/demos/wasserwaage/MainActivity.java
+++ app/src/main/java/de/ur/mi/android/demos/wasserwaage/MainActivity.java
@@ -1,68 +1,34 @@
 package de.ur.mi.android.demos.wasserwaage;
 
-import android.content.Context;
-import android.hardware.SensorManager;
 import android.os.Bundle;
-import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
+/**
+ * Die Wasserwaage: Eine interkative Wasserwage für Android-Smartphones
+ * <p>
+ * Diese App stellt eine digitale Wasserwage zur Verfügung. Im User Interface werden zwei separate
+ * Elemente für die horizontale und vertikale Ausrichtung des Geräts angezeigt. Auf Basis der
+ * aktuellen Werte des Beschleunigungssensors werden die jeweiligen Inidikatoren der beiden Elemente
+ * positioniert, um die aktuelle Lage des Geräts zu visualisieren.
+ * <p>
+ * Die Genauigkeit der Anzeige wird durch eine Möglichkeit zur Kalibrierung der Werte erhöht:
+ * Nutzer*innen können das Gerät auf einer planen Fläche ablegen und über einen Klick auf den
+ * Kalibrierungs-Button die aktuellen Messwerte speichern. Diese werden bei allen nachfolgenden
+ * Messungen zur Normalisierung der Sensordaten verwendet.
+ * <p>
+ * Für die Darstellung der aktuellen Geräteausrichtung sind bereits zwei passende CustomViews im
+ * Layout der Activity ergänzt. Diese basieren auf der Klasse SpiritLevel, werden in der XML-Datei
+ * des Layouts eingefügt und konfiguriert und können hier in der Activity wie alle anderen bekannten
+ * Views verwendet werden.
+ */
+public class MainActivity extends AppCompatActivity {
 
-import de.ur.mi.android.demos.wasserwaage.sensors.OrientationProvider;
-import de.ur.mi.android.demos.wasserwaage.ui.SpiritLevelView;
-
-public class MainActivity extends AppCompatActivity implements OrientationProvider.OrientationListener {
-
-    private OrientationProvider provider;
-    private SpiritLevelView spiritLevelHorizontal;
-    private SpiritLevelView spiritLevelVertical;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        initUI();
-        initOrientationProvider();
-    }
-
-    private void initUI() {
         setContentView(R.layout.activity_main);
-        FloatingActionButton fab = findViewById(R.id.calibrate_values_fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (provider != null) {
-                    provider.calibrate();
-                }
-            }
-        });
-        spiritLevelHorizontal = findViewById(R.id.spirit_level_view_horizontal);
-        spiritLevelVertical = findViewById(R.id.spirit_level_view_vertical);
     }
 
-    private void initOrientationProvider() {
-        SensorManager sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-        provider = new OrientationProvider(sensorManager, this);
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        provider.calibrate();
-        provider.start();
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        provider.stop();
-    }
-
-    @Override
-    public void onOrientationChanged(float x, float y, float z) {
-        float bubblePositionForVerticalLevel = 0.5f - (y / 10);
-        float bubblePositionForHorizontalLevel = 0.5f - (x / 10);
-        spiritLevelVertical.setBubblePosition(bubblePositionForVerticalLevel);
-        spiritLevelHorizontal.setBubblePosition(bubblePositionForHorizontalLevel);
-    }
 }
