14250670d84e7f1404ba35ce9391d4cb69ddc462
Alexander Bazo <alexanderbazo@googlemail.com>
2021-06-17 12:30:22 +0200

    Add comments

--- app/src/main/java/de/ur/mi/android/demos/wasserwaage/MainActivity.java
+++ app/src/main/java/de/ur/mi/android/demos/wasserwaage/MainActivity.java
@@ -12,9 +12,33 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import de.ur.mi.android.demos.wasserwaage.sensors.OrientationProvider;
 import de.ur.mi.android.demos.wasserwaage.ui.SpiritLevelView;
 
+/**
+ * Die Wasserwaage: Eine interaktive Wasserwaage für Android-Smartphones
+ * <p>
+ * Diese App stellt eine digitale Wasserwaage zur Verfügung. Im User Interface werden zwei separate
+ * Elemente für die horizontale und vertikale Ausrichtung des Geräts angezeigt. Auf Basis der
+ * aktuellen Werte des Beschleunigungssensor werden die jeweiligen Indikatoren der beiden Elemente
+ * positioniert, um die aktuelle Lage des Geräts zu visualisieren.
+ * <p>
+ * Die Genauigkeit der Anzeige wird durch eine Möglichkeit zur Kalibrierung der Werte erhöht:
+ * Nutzer*innen können das Gerät auf einer planen Fläche ablegen und über einen Klick auf den
+ * Kalibrierungs-Button die aktuellen Messwerte speichern. Diese werden bei allen nachfolgenden
+ * Messungen zur Normalisierung der Sensordaten verwendet.
+ */
 public class MainActivity extends AppCompatActivity implements OrientationProvider.OrientationListener {
 
+    /**
+     * In der selbst-erstellten Klasse OrientationProvider werden alle Funktionen gebündelt, die
+     * direkt den Umgang mit dem Beschleunigungssensor betreffen. Instanzen der Klasse kommunizieren
+     * die aktuellen Messwerte des Sensors über eine Listener-Schnittstelle (OrientationListener)
+     * mit einer entsprechend registrierten, anderen Komponenten.
+     */
     private OrientationProvider provider;
+    /**
+     * Die beiden UI-Elemente zur Darstellung der horizontalen und vertikalen Wasserwaagen werden
+     * in zwei entsprechenden Instanzvariablen gehalten. Als Datentyp dient der Klassenname des
+     * Custom View.
+     */
     private SpiritLevelView spiritLevelHorizontal;
     private SpiritLevelView spiritLevelVertical;
 
@@ -25,6 +49,12 @@ public class MainActivity extends AppCompatActivity implements OrientationProvid
         initOrientationProvider();
     }
 
+    /**
+     * Die Methode initialisiert das User Interface der Activity. Dabei wird der Floating Action
+     * Button mit einem Click-Listener versehen, der bei Interaktion der Nutzer*innen mit dem Button
+     * die Kalibrierungsfunktion der App auslöst. Zusätzlich werden die beiden UI-Elemente für die
+     * Wasserwaagen referenziert und in den entsprechenden Instanzvariablen gespeichert.
+     */
     private void initUI() {
         setContentView(R.layout.activity_main);
         FloatingActionButton fab = findViewById(R.id.calibrate_values_fab);
@@ -36,32 +66,89 @@ public class MainActivity extends AppCompatActivity implements OrientationProvid
                 }
             }
         });
+        /*
+         * Custom Views verhalten sich in der Regel genau so, wie "herkömmliche" Views oder
+         * ViewGroups, z.B. wenn sie über die findViewById-Methode anhand ihrer ID aus der Layout-
+         * Datei referenziert und in Form von Java-Objekten innerhalb der App genutzt werden.
+         */
         spiritLevelHorizontal = findViewById(R.id.spirit_level_view_horizontal);
         spiritLevelVertical = findViewById(R.id.spirit_level_view_vertical);
     }
 
+    /**
+     * Die Methode initialisiert den OrientationProvider, der der Activity über eine Listener-
+     * Schnittstelle die Änderungen der Messwerte des Beschleunigungssensors mitteilt. Dieser werden
+     * innerhalb der Activity verwendet, um die UI-Elemente der beiden Wasserwaagen korrekt auszurichten.
+     */
     private void initOrientationProvider() {
+        /*
+         * Innerhalb des Providers wird ein SensorManager benötigt, um auf den relevanten
+         * Beschleunigungssensor zuzugreifen. Statt dem Provider die notwendigen Kontextinformationen
+         * zu übergeben, um den Manager selbstständig referenzieren zu können, erhält der Konstruktor
+         * den bereits erstellten bzw. referenzierten SensorManager als Parameter übergeben. Der
+         * Provider erhält die notwendigen Abhängigkeiten für seine Arbeit, der restliche Kontext
+         * der Activity wird aber nicht unnötig an untergeordnete Komponenten der Anwendung
+         * weitergegeben.
+         */
         SensorManager sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        /*
+         * Zusätzlich zum SensorManager wird ein OrientationListener übergeben, in diesem Fall
+         * implementiert die Activity selbst das entsprechende Interface und registriert sich über
+         * den zweiten Parameter (this) als Listener beim Provider.
+         */
         provider = new OrientationProvider(sensorManager, this);
     }
 
+    /**
+     * Lifecycle-Methode der Activity, die aufgerufen wird, wenn die Activity erstmals, oder nach
+     * dem Verschieben in den Hintergrund, sichtbar wird. Die Aktualisierung des UIs kann nur
+     * innerhalb der aktiven Phase der App durchgeführt werden. Wir synchronisieren den Provider
+     * daher mit diesem Ablauf und Starten an dieser Stelle das Lauschen auf den Beschleunigungssensor.
+     */
     @Override
     protected void onResume() {
         super.onResume();
+        /*
+         * Vor dem Start wird ein Kalibrierungsauftrag an den Provider gestellt, so dass die ersten
+         * Werte des Sensors, die vom Provider empfangen werden, direkt zur Kalibrierung der nachfolgenden
+         * Messwerte verwendet werden können.
+         */
         provider.calibrate();
         provider.start();
     }
 
+    /**
+     * Lifecycle-Methode der Activity, die aufgerufen wird, wenn die Activity in den Hintergrund
+     * verschoben wird. Die Aktualisierung des UIs ist nach diesem Zeitpunkt nicht mehr möglich.
+     * Wir synchronisieren den Provider daher mit diesem Ablauf und Stoppen an dieser Stelle das
+     * Lauschen auf den Beschleunigungssensor.
+     */
     @Override
     protected void onPause() {
         super.onPause();
         provider.stop();
     }
 
+    /**
+     * Callback-Methode aus dem Listener-Interface (OrientationProvider) die aufgerufen wird, wenn
+     * der Provider neue Messwerte vom Beschleunigungssensor erhalten hat.
+     *
+     * @param x Aktuell gemessene (normalisierte) Beschleunigung auf der X-Achse
+     * @param y Aktuell gemessene (normalisierte) Beschleunigung auf der Y-Achse
+     * @param z Aktuell gemessene (normalisierte) Beschleunigung auf der Z-Achse
+     */
     @Override
-    public void onOrientationChanged(float x, float y, float z) {
-        float bubblePositionForVerticalLevel = 0.5f - (y / 10);
-        float bubblePositionForHorizontalLevel = 0.5f - (x / 10);
+    public void onNewMeasurementAvailable(float x, float y, float z) {
+        /*
+         * Innerhalb des Wertebereichs des OrientationProvider markiert ein Wert um 0 die Ruheposition
+         * bzw. in dieser App ein, auf der entsprechenden Achse, plan ausgerichtetes Gerät. In den
+         * UI-Elementen wird dieser Zustand durch die Positionierung der Luftblase der Wasserwaage an
+         * der Stelle 0.5 repräsentiert. Hier erfolgt die entsprechende Umrechnung der Werte. Der
+         * Messwert des Sensors wird mit 10 multipliziert, da Unterschiede sonst nur bei Extremwerten
+         * sichtbar wären.
+         */
+        float bubblePositionForVerticalLevel = 0.5f - (y * 10);
+        float bubblePositionForHorizontalLevel = 0.5f - (x * 10);
         spiritLevelVertical.setBubblePosition(bubblePositionForVerticalLevel);
         spiritLevelHorizontal.setBubblePosition(bubblePositionForHorizontalLevel);
     }
--- app/src/main/java/de/ur/mi/android/demos/wasserwaage/sensors/OrientationProvider.java
+++ app/src/main/java/de/ur/mi/android/demos/wasserwaage/sensors/OrientationProvider.java
@@ -7,10 +7,14 @@ import android.hardware.SensorManager;
 
 public class OrientationProvider implements SensorEventListener {
 
+    // SensorManager für die Auswahl des Beschleunigungssensors, wird im Konstruktor übergeben und
     private final SensorManager sensorManager;
     private final OrientationListener listener;
+    // Messwerte des Sensors zum Zeitpunkt der letzten Kalibrierung
     private final float[] calibrationValues = new float[3];
+    // Zu letzt erhaltene Messwerte des Sensors
     private final float[] currentValues = new float[3];
+    // Indikator, der aussagt, ob beim nächsten Update der Sensorwerte eine Kalibrierung durchgeführt werden soll
     private boolean calibrationRequestPending = false;
 
     public OrientationProvider(SensorManager sensorManager, OrientationListener listener) {
@@ -18,49 +22,105 @@ public class OrientationProvider implements SensorEventListener {
         this.listener = listener;
     }
 
+    /**
+     * Startet die Verarbeitung und Weitergabe der Sensorwerte des Beschleunigungssensors
+     */
     public void start() {
+        /**
+         * In der Methode wird versucht, den Beschleunigungssensor des Systems auszulesen und die
+         * aktuelle Instanz des OrientationProviders als Listener für Sensorupdates auf diesem zu
+         * registrieren.
+         */
         Sensor accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
         if (accelerometer != null) {
             sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL, SensorManager.SENSOR_DELAY_NORMAL);
         }
     }
 
+    /**
+     * Stoppt die Verarbeitung und Weitergabe der Sensorwerte des Beschleunigungssensors
+     */
     public void stop() {
+        // Deregistriert diese Instanz des OrientationProviders als SensorListener
         sensorManager.unregisterListener(this);
     }
 
+    /**
+     * Führt beim nächsten Update der Sensorwerte eine Kalibrierung durch, die sich auf alle danach
+     * aus dem Provider weitergegebenen Messwerte auswirkt.
+     */
     public void calibrate() {
         calibrationRequestPending = true;
     }
 
+    /**
+     * Callback-Methode die aufgerufen wird, wenn der (Beschleunigungs-) Sensor neue Messwerte bereitstellt
+     *
+     * @param event Informationen zur Messung, z.B. die genauen Werte
+     */
     @Override
     public void onSensorChanged(SensorEvent event) {
+        // Kopiert die aktuellen Messwerte in den Zwischenspeicher
         System.arraycopy(event.values, 0, currentValues, 0, currentValues.length);
+        // Falls im Vorfeld eine Kalibrierung vorgemerkt wurde ...
         if (calibrationRequestPending) {
+            // ... werden die aktuellen Messwerte zusätzlich als Kalibrierungswerte gespeichert ...
             System.arraycopy(currentValues, 0, calibrationValues, 0, calibrationValues.length);
+            // ... und der Kalibrierungsauftrag wird als abgeschlossen markiert
             calibrationRequestPending = false;
         }
-        float[] normalizedValues = getNormalizedValues(currentValues, 10);
-        listener.onOrientationChanged(normalizedValues[0], normalizedValues[1], normalizedValues[2]);
+        /*
+         * Der maximale Wert, der aus dem eingesetzten Sensor ausgelesen werden kann, wird über
+         * die Methode getMaximumRange bestimmt. Der minimale Wert entspricht dem negierten
+         * Maximalwert. Dieser Wert wird verwendet, um alle Messwerte auf die Skala -1.0 bis 1.0 zu
+         * reduzieren. Der Wert wird an die Normalisierungsfunktion übergeben, in der dieser, zusammen
+         * mit möglicherweise vorhandenen Kalibrierungswerten, zur Anpassung der aktuellsten Messwerte
+         * des Sensors verwendet wird.
+         */
+        float[] normalizedValues = getNormalizedValues(currentValues, event.sensor.getMaximumRange());
+        // Die normalisierten Werden werden anschließend an den Listener weitergeben (in diesem Fall: MainActivity)
+        listener.onNewMeasurementAvailable(normalizedValues[0], normalizedValues[1], normalizedValues[2]);
     }
 
-    private float[] getNormalizedValues(float[] rawValues, int precisionBase) {
+    /**
+     * Normalisiert die übergebenen Messwerte anhand verschiedener Kritierien und gibt ein Array
+     * mit den so umgewandelten Werten zurück.
+     */
+    private float[] getNormalizedValues(float[] rawValues, float normalizingDivisor) {
+        // "Leeres" Array für die in dieser Methode normalisierten Messwerte
         float[] normalizedValues = new float[rawValues.length];
+        // Kopieren der "rohen" Messwerte in das Zielarray
         System.arraycopy(rawValues, 0, normalizedValues, 0, normalizedValues.length);
+        // Für jeden Messwert ...
         for (int i = 0; i < normalizedValues.length; i++) {
+            // ... wird der entsprechende Kalibrierungswert (Ruheposition) abgezogen ...
             float calibratedValue = rawValues[i] - calibrationValues[i];
-            int tmp = (int) (calibratedValue * precisionBase);
-            normalizedValues[i] = ((float) tmp) / precisionBase;
+            // ... und das Ergebnis durch Division mit dem maximalen Sensorwert auf eine passende Skala runtergebrochen
+            normalizedValues[i] = calibratedValue / normalizingDivisor;
         }
         return normalizedValues;
     }
 
+    /**
+     * Callback-Methode die aufgerufen wird, wenn sich die Genauigkeit des eingesetzten Sensors
+     * ändert (wird hier nicht verwendet)
+     */
     @Override
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
-
     }
 
+    /**
+     * Interface für Listener, die über Änderungen an den Messwerten des Beschleunigungssensors
+     * informiert werden sollen.
+     */
     public interface OrientationListener {
-        void onOrientationChanged(float x, float y, float z);
+        /**
+         * Die Methode wird aufgerufen, wenn neue Messwerte des Beschleunigungssensors vorliegen.
+         *
+         * @param x Aktuelle Beschleunigung auf der X-Achse, normalisiert für den Wertebereich -1.0 bis 1.0
+         * @param y Aktuelle Beschleunigung auf der Y-Achse, normalisiert für den Wertebereich -1.0 bis 1.0
+         * @param z Aktuelle Beschleunigung auf der Z-Achse, normalisiert für den Wertebereich -1.0 bis 1.0
+         */
+        void onNewMeasurementAvailable(float x, float y, float z);
     }
 }
